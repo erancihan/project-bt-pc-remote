c7b9c8cb94c12178a339813a7b20fcf8dd2ee8a1
Cihan Eran <erancihan@gmail.com>
2017-06-02 04:09:48 +0300

    add imports

---
+++ bt-mouse-client/app/src/main/java/sleepless/bt_mouse_client/BluetoothIO.java
@@ -0,0 +1,16 @@
+package sleepless.bt_mouse_client;
+
+import android.os.Handler;
+
+/**
+ * Created by freedrone on 02.06.2017.
+ */
+
+class BluetoothIO {
+    public BluetoothIO(MainActivity mainActivity, Handler mHandler) {
+    }
+
+    public void stop() {
+
+    }
+}
